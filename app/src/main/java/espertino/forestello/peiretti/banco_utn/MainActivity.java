b72eafa3035a71b4e78978529b49ae00a7adac7f
clari62 <clariespertino62@gmail.com>
2022-09-09 20:55:19 -0300

    Creación pantalla construir plazo fijo

--- app/src/main/java/espertino/forestello/peiretti/banco_utn/MainActivity.java
+++ app/src/main/java/espertino/forestello/peiretti/banco_utn/MainActivity.java
@@ -3,12 +3,28 @@ package espertino.forestello.peiretti.banco_utn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import espertino.forestello.peiretti.banco_utn.databinding.ActivityConstruirPlazoFijoBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private ArrayAdapter<CharSequence> monedas;
+    private ActivityConstruirPlazoFijoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding =  ActivityConstruirPlazoFijoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        spinner = (Spinner) binding.spinnerMoneda;
+        monedas = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,getResources().getStringArray(R.array.monedas));
+        spinner.setAdapter(monedas);
+
     }
 }
